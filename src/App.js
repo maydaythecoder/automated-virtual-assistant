2c2e0cfcb8459bad4678524d6b2c01a70ffe1d64
mayday <maydaythecoder@gmail.com>
2023-08-27 18:14:33 -0400

    save v-1 prototype

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Main from './Components/Main/Main';
+import Cards from './Components/Cards/Cards';
 
+
+/*
+TODO: Incorporate AI Assistant
+TODO: Add email authentication to make user Profiles
+TODO: DataBase To store user info
+TODO: feed user info back into the ai and save patterns in the database, continue this loop to improve user experience
+
+*/
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Main />
+      <Cards />
     </div>
   );
 }
--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./Cards.css"
 
+
+/*
+TODO: Make Card Data Dynamic: Json File
+TODO: add 3 cards, 1 for calender/ 1 for socials/ 1 for News based off user preferences|
+*/
 const Cards = () => {
   return (
-    <div>
-      
+    <div className='Card'>
+      <h1 id='Card-Header' >Header</h1>
+      <h6 id='Card-Footer' >Footer</h6>
+      <button id='Card-Button' >Action</button>
     </div>
   )
 }
