d41c3f5f9280bc228a6535b933435b7c69f81945
mayday <maydaythecoder@gmail.com>
2023-08-28 16:35:24 -0400

    setup for text effect

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -8,11 +8,23 @@ TODO: add 3 cards, 1 for calender/ 1 for socials/ 1 for News based off user pref
 */
 const Cards = () => {
   return (
+  <div>
     <div className='Card'>
       <h1 id='Card-Header' >Header</h1>
-      <h6 id='Card-Footer' >Footer</h6>
+        <h6 id='Card-Footer' >Footer</h6>
       <button id='Card-Button' >Action</button>
     </div>
+     <div className='Card2'>
+      <h1 id='Card-Header2' >Header</h1>
+        <h6 id='Card-Footer2' >Footer</h6>
+      <button id='Card-Button2' >Action</button>
+    </div>
+    <div className='Card3'>
+      <h1 id='Card-Header3' >Header</h1>
+        <h6 id='Card-Footer3' >Footer</h6>
+      <button id='Card-Button3' >Action</button>
+    </div>
+  </div>
   )
 }
 
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,10 +1,51 @@
 import React from 'react'
 import "./Main.css"
 
+let Name = ["User"]
+
+//split name
+//start as hidden
+//reveal one letter at a time
+
+/*
+
+//on hover use effect from parallax file
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index <  iteration ) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+
+    if(iteration >= event.target.dataset.value.length){
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+}
+
+*/
+
 const Main = () => {
   return (
     <div>
-      <h1 className='Header'>Hello User</h1>
+      <h1 className='Header'>Hello {Name}</h1>
       <h6 className='Caption'>How can i help you</h6>
       <p className='dash' id='left'>l</p>
       <p className='dash' id='middle'>l</p>
